2d6948f50c1785f67daaefabd62143be5f17ef23
e.lukin <e.lukin@simcase.ru>
2017-10-06 18:27:37 +0400

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
   post 'api/v1/user_token' => 'api/v1/user_token#create'
-  root 'test#index'
+  root 'web/dashboard#index'
+  get 'signin' => 'web/dashboard#index'
+  get 'signup' => 'web/dashboard#index'
 
   namespace :api do
     namespace :v1 do
-      resources :posts, only: %i[index create show] do
+      resources :posts, only: %i[index show create] do
         resources :comments, only: %i[index create]
+      end
+
+      resources :users, only: %i[show create update] do
+        collection do
+          get :auth
+        end
       end
     end
   end

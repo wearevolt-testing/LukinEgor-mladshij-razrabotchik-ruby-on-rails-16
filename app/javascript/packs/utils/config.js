081c3488c710e657696ce2ba0287894b760d5689
e.lukin <e.lukin@simcase.ru>
2017-10-06 19:04:19 +0400

    add prodHost to client settings

--- app/javascript/packs/utils/config.js
+++ app/javascript/packs/utils/config.js
@@ -1,6 +1,7 @@
 export default class Config {
   static url() {
     const devHost = 'http://localhost:3000';
+    const prodHost = 'https://wearevolt-testing.herokuapp.com'
 
     if (process.env.NODE_ENV === 'production') {
       return prodHost
